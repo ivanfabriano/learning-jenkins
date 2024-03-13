33909173f40e12045e85b3de06427ed27aa8b6e3
ivanfabriano <ivanfabrianos@gmail.com>
2024-03-13 09:31:04 +0700

    Test change

--- src/server.ts
+++ src/server.ts
@@ -7,5 +7,5 @@ dotenv.config();
 app.listen(process.env.PORT, () => console.log(`Server is running in port ${process.env.PORT}`));
 
 app.get('/', (req: Request, res: Response) => {
-    return res.status(200).send('Node example for image builder is working now')
+    return res.status(200).send('Node example for image builder is working now yeay')
 })
